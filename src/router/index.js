acff5886c0c4fe6b0c2d76bd337e085871a5ad52
maudhelen <mhovland.ieu2021@student.ie.edu>
2023-10-11 16:41:19 +0200

    import new vue component

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Skull from '../components/Skull.vue'
 import AppAccounts from '../components/AppAccounts.vue'
+import welcome from '../components/welcome.vue'
 
 Vue.use(VueRouter)
 
