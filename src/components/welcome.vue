f70f6a35068b6e22e26e6005942e20f46076ddd4
maudhelen <mhovland.ieu2021@student.ie.edu>
2023-10-11 16:44:24 +0200

    add space between links

--- src/components/welcome.vue
+++ src/components/welcome.vue
@@ -3,6 +3,7 @@
       <h1>Welcome to My Vue App</h1>
       <p>This is a simple welcome message.</p>
       <router-link to="/accounts">Accounts</router-link>
+      <br>
       <router-link to="/skull">Check out Skull</router-link>
     </div>
   </template>
