547f8cd2e174a10dfd2a0e13dad9112ff7ba7441
maudhelen <mhovland.ieu2021@student.ie.edu>
2023-10-11 16:42:52 +0200

    edit endpoints

--- src/components/welcome.vue
+++ src/components/welcome.vue
@@ -2,8 +2,8 @@
     <div>
       <h1>Welcome to My Vue App</h1>
       <p>This is a simple welcome message.</p>
-      <router-link to="/page1">Go to Page 1</router-link>
-      <router-link to="/page2">Go to Page 2</router-link>
+      <router-link to="/accounts">Accounts</router-link>
+      <router-link to="/skull">Check out Skull</router-link>
     </div>
   </template>
   
