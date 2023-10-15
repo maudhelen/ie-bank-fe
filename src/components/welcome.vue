5a1d34c26a0f2466e84b08b5b7f7cd01e61faf20
maudhelen <mhovland.ieu2021@student.ie.edu>
2023-10-15 23:37:11 +0200

    hello

--- src/components/welcome.vue
+++ src/components/welcome.vue
@@ -1,16 +1,31 @@
 <template>
-    <div>
-      <h1>Welcome to My Vue App</h1>
-      <p>This is a simple welcome message.</p>
-      <router-link to="/accounts">Accounts</router-link>
-      <br>
-      <router-link to="/skull">Check out Skull</router-link>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'Welcome',
-  };
-  </script>
-  
+  <div class="centered-content">
+    <h1>Welcome to IE Bank</h1>
+    <p>Feel free to check out this.</p>
+    <router-link to="/accounts">Accounts</router-link>
+    <br>
+    <router-link to="/skull">Check out Skull</router-link>
+  </div>
+</template>
+
+<style scoped>
+.centered-content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh; /* This makes the content take the full viewport height. */
+  text-align: center;
+}
+
+.centered-content h1 {
+  font-size: 36px;
+  color: #333;
+}
+
+.centered-content p {
+  font-size: 18px;
+  color: #555;
+  margin-top: 20px;
+}
+</style>
